6733f9e053aca9b9e7a6a53eb4d921da1e2f762c
Aicha Mbongo <zindamoyen2@gmail.com>
2024-04-02 20:08:24 +0300

    refactor: Wrap JSX elements in a shared parent components To adhere to JSX syntax requirements, wrap the JSX elements within a shared parent component (a <div> or empty <>..</> element)

--- quick_start-app/src/components/MyButton.tsx
+++ quick_start-app/src/components/MyButton.tsx
@@ -8,8 +8,14 @@ function MyButton () {
 // Nest the button into another component
 export default function MyApp() {
     return (
+
+        // JSX is stricter than HTML. You have to close tags like 
+        // <br />. Your component also can’t return multiple JSX tags. 
+        // You have to wrap them into a shared parent, like a <div>...</div> 
+        // or an empty <>...</> wrapper:
         <div>
             <h1>Have a look at my app!</h1>
+            <p>For more details click the button below</p>
             <MyButton />
         </div>
     )
