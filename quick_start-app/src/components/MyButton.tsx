abf4abed1c98ab24506b2492d5a3f4fb7e4c4ba5
Aicha Mbongo <zindamoyen2@gmail.com>
2024-04-02 18:48:27 +0300

    feat: Add MyApp component and integrate MyButton Adda new functional component MyApp that serves as the main entry point of the app.

--- quick_start-app/src/components/MyButton.tsx
+++ quick_start-app/src/components/MyButton.tsx
@@ -1,6 +1,16 @@
+// Declare MyButton
 function MyButton () {
     return (
         <button> This is a button</button>
     );
 }
 
+// Nest the button into another component
+export default function MyApp() {
+    return (
+        <div>
+            <h1>Have a look at my app!</h1>
+            <MyButton />
+        </div>
+    )
+}
