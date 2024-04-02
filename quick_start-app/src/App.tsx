735a287f1c9088a183e7a42702e32042b93b82e3
Aicha Mbongo <zindamoyen2@gmail.com>
2024-04-02 19:46:14 +0300

    feat: Integrate MyApp component into App
    Integrate the MyApp component into the App component. MyApp serves as the main content of the application, wrapped within a div with the class name "App".

--- quick_start-app/src/App.tsx
+++ quick_start-app/src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MyButton from './components/MyButton';
+import MyApp from './components/MyButton';
 
 function App() {
   return (
     <div className="App">
-      <MyButton />
+      <MyApp />
     </div>
   );
 }
